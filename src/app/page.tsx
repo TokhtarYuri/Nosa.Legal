2688e81fa5c4e98795587ae2245a6027cb1e75bd
Tokhtar <jura.tokhtar@gmail.com>
2024-12-18 16:42:07 +0200

    Initial commit: Next.js landing setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Welcome to Our Landing Page</h1>
+        <p>Start building your awesome Next.js project!</p>
+      </main>
+    </div>
+  );
+}
+
